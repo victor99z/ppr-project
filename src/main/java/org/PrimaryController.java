e91297422a101c89344d3a91bfc6baf7fe3b218f
Draylon Vieira <draylid@gmail.com>
2019-11-01 14:35:25 -0300

    Primeira alteração do fxml

--- src/main/java/org/PrimaryController.java
+++ src/main/java/org/PrimaryController.java
@@ -2,8 +2,12 @@ package org;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+    @FXML
+    private Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
--- src/main/java/org/SecondaryController.java
+++ src/main/java/org/SecondaryController.java
@@ -2,8 +2,12 @@ package org;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class SecondaryController {
+
+    @FXML
+    private Button secondaryButton;
 
     @FXML
     private void switchToPrimary() throws IOException {
