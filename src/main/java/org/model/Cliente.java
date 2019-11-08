107a40278dad0a87194fcfbd177093b60e3fa984
Victor Bernardes <victor_luizbernardes5@outlook.com>
2019-11-08 00:49:14 -0300

    add classes

---
+++ src/main/java/org/model/Cliente.java
@@ -0,0 +1,15 @@
+package org.model;
+
+public class Cliente {
+
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+}
