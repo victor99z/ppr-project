34197e6ca6dd77e76818171d0ece398fbf008abe
jubileu99 <victor_luizbernardes5@outlook.com>
2019-11-29 19:10:15 -0200

    update verificaCpf

--- src/main/java/org/view/SecondaryController.java
+++ src/main/java/org/view/SecondaryController.java
@@ -1,13 +1,38 @@
 package org.view;
 
 import java.io.IOException;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 import org.App;
 
 public class SecondaryController {
 
     @FXML
+    private Label labelErro;
+
+    @FXML
+    private JFXTextField input;
+
+    @FXML
+    private JFXButton secondaryButton;
+
+    @FXML
     private void switchToMenu() throws IOException {
-        App.setRoot("menu");
+        App.setRoot("Menu");
     }
+
+    @FXML
+    void verificaCpf(ActionEvent event) throws IOException{
+        if(input.getText().length() == 11){
+            App.setRoot("Menu");
+        }else{
+            System.err.println("CPF INVALIDO");
+            input.setPromptText("CPF Invalido");
+        }
+    } 
 }
